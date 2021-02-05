98cb79ac85cb8cc9c31f11b7c866c52b3c51eed2
benantonyjackson <benantonyjackson@gmail.com>
2021-02-05 18:56:16 +0000

    Updated unit tests

--- src/backend/tests/test_retrieve_test.py
+++ src/backend/tests/test_retrieve_test.py
@@ -23,16 +23,21 @@ def test_no_pickups_to_return(client, db):
     res = client.get("/api/test", headers={'Authorization': "Bearer " + token},
                      query_string={"test_id": "testUUID"})
 
+    assert "drug_id" not in str(res.json)
+
     assert res.status_code == 404
 
+
+def test_id_not_found(client, db):
     # Record to accommodate for foreign key constraints
     db.session.execute('INSERT INTO drugs () VALUES ("drugUUID", "Drug 1")')
     # Record that should not be returned
     db.session.execute('INSERT INTO tests () VALUES ("testUUID", "drugUUID", "INCOMPLETE")')
 
+    token = auth.get_access_token(client, db, "test_user", "test_password", "technician")
+
     res = client.get("/api/test", headers={'Authorization': "Bearer " + token},
                      query_string={"test_id": "not-test-id"})
 
     assert "drug_id" not in str(res.json)
-
     assert res.status_code == 404
