9f4fdd27f7bca0a30c2af764401baa2177b01d35
Matthew <N0743797@my.ntu.ac.uk>
2021-02-08 15:22:07 +0000

    Clean Up Pages

--- src/frontend/src/routes.js
+++ src/frontend/src/routes.js
@@ -2,25 +2,20 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import DashboardLayout from './layouts/DashboardLayout';
 import MainLayout from './layouts/MainLayout';
-import AccountView from './views/account/AccountView';
-import CustomerListView from './views/customer/CustomerListView';
 import DashboardView from './views/reports/DashboardView';
 import LoginView from './views/auth/LoginView';
 import NotFoundView from './views/errors/NotFoundView';
-import ProductListView from './views/product/ProductListView';
-import RegisterView from './views/auth/RegisterView';
-import SettingsView from './views/settings/SettingsView';
+import PatientListView from './views/patient/PatientListView';
+import PickupListView from './views/pickup/PickupListView';
 
 const routes = [
   {
     path: 'app',
     element: <DashboardLayout />,
     children: [
-      { path: 'account', element: <AccountView /> },
-      { path: 'customers', element: <CustomerListView /> },
+      { path: 'patients', element: <PatientListView /> },
       { path: 'dashboard', element: <DashboardView /> },
-      { path: 'products', element: <ProductListView /> },
-      { path: 'settings', element: <SettingsView /> },
+      { path: 'pickups', element: <PickupListView /> },
       { path: '*', element: <Navigate to="/404" /> }
     ]
   },
@@ -29,7 +24,6 @@ const routes = [
     element: <MainLayout />,
     children: [
       { path: 'login', element: <LoginView /> },
-      { path: 'register', element: <RegisterView /> },
       { path: '404', element: <NotFoundView /> },
       { path: '/', element: <Navigate to="/app/dashboard" /> },
       { path: '*', element: <Navigate to="/404" /> }
