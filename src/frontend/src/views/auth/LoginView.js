c04202bb032beebc41b5379c08ed809570982f08
georgehanlon <georgehanlon@live.com>
2021-02-12 15:32:54 +0000

    Updated validation for fields

--- src/frontend/src/views/auth/LoginView.js
+++ src/frontend/src/views/auth/LoginView.js
@@ -42,11 +42,11 @@ const LoginView = () => {
         <Container maxWidth="sm">
           <Formik
             initialValues={{
-              email: 'admin',
+              username: 'admin',
               password: 'MasterMonkey'
             }}
             validationSchema={Yup.object().shape({
-              email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
+              username: Yup.string().max(255).required('Username is required'),
               password: Yup.string().max(255).required('Password is required')
             })}
             onSubmit={() => {
@@ -79,16 +79,16 @@ const LoginView = () => {
                   </Typography>
                 </Box>
                 <TextField
-                  error={Boolean(touched.email && errors.email)}
+                  error={Boolean(touched.username && errors.username)}
                   fullWidth
-                  helperText={touched.email && errors.email}
-                  label="Email Address"
+                  helperText={touched.username && errors.username}
+                  label="Username"
                   margin="normal"
-                  name="email"
+                  name="username"
                   onBlur={handleBlur}
                   onChange={handleChange}
-                  type="email"
-                  value={values.email}
+                  type="text"
+                  value={values.username}
                   variant="outlined"
                 />
                 <TextField
@@ -109,7 +109,7 @@ const LoginView = () => {
                     color="primary"
                     disabled={isSubmitting}
                     fullWidth
-                    onClick={isAuthenticated(values.email, values.password)}
+                    onClick={isAuthenticated(values.username, values.password)}
                     size="large"
                     type="submit"
                     variant="contained"
