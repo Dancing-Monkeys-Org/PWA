6af7ee7c8491d87706910dc393adf56e71bd9ec5
benantonyjackson <benantonyjackson@gmail.com>
2021-02-18 18:07:08 +0000

    Fixed failing unit tests

--- src/backend/tests/test_pickups.py
+++ src/backend/tests/test_pickups.py
@@ -1,10 +1,15 @@
 from . import auth
 
+
 def test_return_empty(db, client):
     token = auth.get_access_token(client, db, "testUser", "password", "pharmacist")
-    res = client.get("/api/pickups", headers={'Authorization': "Bearer " + token})
+    res = client.get("/api/pickups", headers={'Authorization': "Bearer " + token},
+                     query_string={"pickup_status": "AWAITING_PHARMACIST_AUTHORISATION",
+                                   "scheduled_before": "2022-01-01"}
+                     )
     assert res.json == []
     assert res.status_code == 200
+
 
 def test_return_one(db, client):
     token = auth.get_access_token(client, db, "testUser", "password", "pharmacist")
@@ -14,10 +19,15 @@ def test_return_one(db, client):
     db.session.execute('INSERT INTO patients() VALUES ("patientUUID", "gpsUUID", "sensitivityUUID", "Ben", "Jackson", "F", 12, "contactDetailUUID")')
     db.session.execute('INSERT INTO drugs () VALUES ("drugUUID", "Drug 1")')
     db.session.execute('INSERT INTO standardtests () VALUES ("testUUID", "Blood test 1")')
-    db.session.execute('INSERT INTO medicalpickups () VALUES ("medicalPickupUUID", "patientUUID", "drugUUID", 1, 2021-02-01, 2021-02-01, 1, "AWAITING_PICKUP")')
-    res = client.get("/api/pickups", headers={'Authorization': "Bearer " + token})
+    db.session.execute('INSERT INTO medicalpickups () VALUES ("medicalPickupUUID", "patientUUID", "drugUUID", 1, 2021-02-01, 2021-02-01, 1, "AWAITING_PHARMACIST_AUTHORISATION")')
+
+    res = client.get("/api/pickups", headers={'Authorization': "Bearer " + token},
+                     query_string={"pickup_status": "AWAITING_PHARMACIST_AUTHORISATION",
+                                   "scheduled_before": "2022-01-01"})
+
     assert res.json[0]["pickup_id"] == 'medicalPickupUUID'
     assert res.status_code == 200
+
 
 def test_return_multiple(db, client):
     token = auth.get_access_token(client, db, "testUser", "password", "pharmacist")
@@ -33,9 +43,13 @@ def test_return_multiple(db, client):
     db.session.execute('INSERT INTO drugs () VALUES ("drugUUID2", "Drug 1")')
     db.session.execute('INSERT INTO standardtests () VALUES ("testUUID", "Blood test 1")')
     db.session.execute('INSERT INTO standardtests () VALUES ("testUUID2", "Blood test 2")')
-    db.session.execute('INSERT INTO medicalpickups () VALUES ("medicalPickupUUID", "patientUUID", "drugUUID", 1, 2021-02-01, 2021-02-01, 1, "AWAITING_PICKUP")')
-    db.session.execute('INSERT INTO medicalpickups () VALUES ("medicalPickupUUID2", "patientUUID2", "drugUUID2", 1, 2021-02-01, 2021-02-01, 1, "AWAITING_PICKUP")')
-    res = client.get("/api/pickups", headers={'Authorization': "Bearer " + token})
+    db.session.execute('INSERT INTO medicalpickups () VALUES ("medicalPickupUUID", "patientUUID", "drugUUID", 1, 2021-02-01, 2021-02-01, 1, "AWAITING_PHARMACIST_AUTHORISATION")')
+    db.session.execute('INSERT INTO medicalpickups () VALUES ("medicalPickupUUID2", "patientUUID2", "drugUUID2", 1, 2021-02-01, 2021-02-01, 1, "AWAITING_PHARMACIST_AUTHORISATION")')
+
+    res = client.get("/api/pickups", headers={'Authorization': "Bearer " + token},
+                     query_string={"pickup_status": "AWAITING_PHARMACIST_AUTHORISATION",
+                                   "scheduled_before": "2022-01-01"})
+
     assert len(res.json) == 2
     assert res.json[0]["pickup_id"] == 'medicalPickupUUID'
     assert res.json[1]["pickup_id"] == 'medicalPickupUUID2'
