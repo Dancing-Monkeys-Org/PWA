04a8b26a4b90bd6b48f8d3d4eda918651df0c485
benantonyjackson <benantonyjackson@gmail.com>
2021-02-18 20:06:27 +0000

    Updated unit test names

--- src/backend/tests/test_patient.py
+++ src/backend/tests/test_patient.py
@@ -7,7 +7,7 @@ def test_not_authorised(client):
     assert "patient_id" not in str(res.json)
 
 
-def test_patient_no_patient_id(client, db):
+def test_no_patient_id_parameter(client, db):
     token = auth.get_access_token(client, db, "test_user", "test_password", "technician")
 
     res = client.get("/api/patient", headers={'Authorization': "Bearer " + token})
@@ -59,7 +59,7 @@ def test_patient_id_not_found(client, db):
     assert res.status_code == 404
 
 
-def test_patient(client, db):
+def test_patient_successfully_retrieved(client, db):
     # Records to accommodate for foreign key constraints
     db.session.execute(
         'INSERT INTO contactdetails () VALUES ("contactDetailUUID", 12345678910, "email1", "addressline11", null,'
